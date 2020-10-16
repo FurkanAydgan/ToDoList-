ae8d603c8d78d9f1be894311fe74816ca0c050d9
Furkan Aydoğan <furkanaydogance@gmail.com>
2020-10-16 12:36:58 +0300

    project files

--- App.js
+++ App.js
@@ -1,35 +1,26 @@
-
-import React, { useState } from 'react';
-import Home from './screens/home';
-import { AppLoading } from 'expo';
-import * as font from 'expo-font';
-import Navigator from './routes/drawer';
+import React, { useState } from "react";
+import { AppLoading } from "expo";
+import * as font from "expo-font";
+import Navigator from "./routes/drawer";
 
 
 
-const getFonts = () => font.loadAsync({
-
-  'Fondamento-Regular': require('./assets/fonts/Fondamento-Regular.ttf'),
-  'Fondamento-Italic': require('./assets/fonts/Fondamento-Italic.ttf')
-});
+const getFonts = () =>
+  font.loadAsync({
+    "Fondamento-Regular": require("./assets/fonts/Fondamento-Regular.ttf"),
+    "Fondamento-Italic": require("./assets/fonts/Fondamento-Italic.ttf"),
+  });
 
 export default function App() {
-
   const [Fontsloaded, setFontsloaded] = useState(false);
+  
+  
 
- 
   if (Fontsloaded) {
-    return (
-       <Navigator/>
-    );
+    return <Navigator />;
   } else {
     return (
-      <AppLoading
-        startAsync={getFonts}
-        onFinish={() => setFontsloaded(true)}
-      />
-    )
+      <AppLoading startAsync={getFonts} onFinish={() => setFontsloaded(true)} />
+    );
   }
-
 }
-
